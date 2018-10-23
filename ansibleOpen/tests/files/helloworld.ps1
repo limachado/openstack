2edfec88f838217dcbbff654a210746177b2f90f
limachado <limachado@indracompany.com>
2018-10-23 11:32:58 -0300

    pla

---
+++ ansibleOpen/tests/files/helloworld.ps1
@@ -0,0 +1,5 @@
+# Filename: helloworld.ps1
+start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList 'http://www.palmeiras.com.br/home/'
+#$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+#New-Item -Path c:\lua -ItemType directory
+# end of script
